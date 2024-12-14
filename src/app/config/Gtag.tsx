d9efc0d9b8c39d406a4bb2dd1556167fc1f0c9d4
Deivid R. S. <deivid@schiitz.com>
2024-12-14 10:40:13 -0400

    Gtag

--- src/app/config/Gtag.tsx
+++ src/app/config/Gtag.tsx
@@ -7,16 +7,15 @@ const Gtag = () => {
         {/*// <!-- Google tag (gtag.js) -->*/}
         <Script strategy="lazyOnload" async src="https://www.googletagmanager.com/gtag/js?id=G-NKTQ32HPT2"/>
         <Script strategy="lazyOnload" id={"gtag"}
-            dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                
-                  gtag('config', 'G-NKTQ32HPT2');
-            `
-            }}
-        />
+        >
+            {
+                `window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-NKTQ32HPT2');`
+            }
+        </Script>
     </div>
 }
 export default Gtag;
