7f2bf6bae3e85fd9e73181d61a69fda4158c27ac
Deivid R. S. <deivid@schiitz.com>
2024-12-14 10:49:58 -0400

    Gtag

--- src/app/config/Gtag.tsx
+++ src/app/config/Gtag.tsx
@@ -1,21 +1,29 @@
-import React from "react";
-import Script from "next/script";
+import  {useEffect} from "react";
+import ReactGA from 'react-ga4';
 
+const TRACKING_ID = "G-NKTQ32HPT2";
 const Gtag = () => {
-    // <!-- Google tag (gtag.js) -->
-    return <div>
-        {/*// <!-- Google tag (gtag.js) -->*/}
-        <Script strategy="lazyOnload" async={true} src="https://www.googletagmanager.com/gtag/js?id=G-NKTQ32HPT2"/>
-        <Script strategy="lazyOnload" id={"gtag"}
-        >
-            {
-                `window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
+    useEffect(() => {
+        ReactGA.initialize(TRACKING_ID);
+        // Send pageview with a custom path
+        ReactGA.send({ hitType: "pageview", page: "/", title: "Home Page" });
+    }, [])
 
-                gtag('config', 'G-NKTQ32HPT2');`
-            }
-        </Script>
-    </div>
+
+    // <!-- Google tag (gtag.js) -->
+    // return <div>
+    //     {/*// <!-- Google tag (gtag.js) -->*/}
+    //     <Script strategy="lazyOnload" async={true} src="https://www.googletagmanager.com/gtag/js?id=G-NKTQ32HPT2"/>
+    //     <Script strategy="lazyOnload" id={"gtag"}
+    //     >
+    //         {
+    //             `window.dataLayer = window.dataLayer || [];
+    //             function gtag(){dataLayer.push(arguments);}
+    //             gtag('js', new Date());
+    //
+    //             gtag('config', 'G-NKTQ32HPT2');`
+    //         }
+    //     </Script>
+    // </div>
 }
 export default Gtag;
