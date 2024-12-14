7fe23e0d99bcb8ad59040fb81aeb9e5640ff68ea
Deivid R. S. <deivid@schiitz.com>
2024-12-14 13:36:48 -0400

    Gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,51 +1,50 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import SquaresBack from "@/app/components/SquaresBack";
 import Header from "@/app/components/Header";
 import React from "react";
-import ReactGA from "react-ga4";
 
-const TRACKING_ID = "G-NKTQ32HPT2";
-ReactGA.initialize(TRACKING_ID);
+import Gtag from "@/app/config/Gtag";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
 const raleway = localFont({
-  src: "./fonts/Raleway.ttf",
-  variable: "--font-raleway"
+    src: "./fonts/Raleway.ttf",
+    variable: "--font-raleway"
 });
 
 export const metadata: Metadata = {
-  title: "Deivid Reinke Schiitz",
-  description: "Deivid Reinke Schiitz Personal Website",
+    title: "Deivid Reinke Schiitz",
+    description: "Deivid Reinke Schiitz Personal Website",
 };
 
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`${raleway.variable} ${geistSans.variable} ${geistMono.variable} antialiased `}>
-      <SquaresBack />
+    return (
+        <html lang="en">
+        <body className={`${raleway.variable} ${geistSans.variable} ${geistMono.variable} antialiased `}>
+        <Gtag/>
+        <SquaresBack/>
         <div className={`z-10 relative flex flex-col h-full w-full overflow-auto`}>
-          <Header/>
+            <Header/>
             {children}
         </div>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
